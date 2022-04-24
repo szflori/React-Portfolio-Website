650a34ff5bf36d12d9513b240f18063e3caa9cd7
Szmolenicki Flórián <h674903@stud.u-szeged.hu>
2022-04-24 15:59:00 +0200

    Sections: About, Experience, Service, Portfolio

--- react-portfolio-website/src/components/Services/Services.jsx
+++ react-portfolio-website/src/components/Services/Services.jsx
@@ -1,9 +1,122 @@
-import React from 'react'
+import React from "react";
+import "./services.css";
+import { BsCheck } from "react-icons/bs";
 
 const Services = () => {
   return (
-    <section id="services">Services</section>
-  )
-}
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
 
-export default Services
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>UI/UX Design</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <BsCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li>
+              <BsCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li>
+              <BsCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li>
+              <BsCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li>
+              <BsCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li>
+              <BsCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li>
+              <BsCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+          </ul>
+        </article>
+        <article className="service">
+          <div className="service__head">
+            <h3>WEB Development</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <BsCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li>
+              <BsCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li>
+              <BsCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li>
+              <BsCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li>
+              <BsCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li>
+              <BsCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li>
+              <BsCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+          </ul>
+        </article>
+        <article className="service">
+          <div className="service__head">
+            <h3>Content Create</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <BsCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li>
+              <BsCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li>
+              <BsCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li>
+              <BsCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li>
+              <BsCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li>
+              <BsCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+            <li>
+              <BsCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet consectetur.</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default Services;
